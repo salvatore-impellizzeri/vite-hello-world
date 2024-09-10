c93cd3da1ed3c67d7db26f6683ae283790b7c532
Salvatore Impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-09-10 09:51:29 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+  data() {
+    return {
+      message: 'Template Vite + Vue'
+    }
+  }
+}
+</script>
+
+<!-- All'interno del template ci va un solo figlio -->
+<template> 
+  <div>
+    <header>
+        <h1> 
+            {{ message }}
+        </h1>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+header {
+    background-color: aquamarine;
+    text-align: center;
+}
+
+h1 {
+    color: lightcoral;
+}
+</style>
+
