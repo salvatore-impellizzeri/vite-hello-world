c468861ad9c5347f65fc42f0feba26202ef72d4b
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-09-10 10:24:27 +0200

    v 1.0.1

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,10 @@
 <script>
-import AppHeader from './components/AppHeader.vue';
 
 export default {
   data() {
     return {
-      message: ''
+      message: 'La mia prima app con Vite!',
     }
-  },
-
-  components: {
-    AppHeader,
   }
 }
 </script>
@@ -17,13 +12,23 @@ export default {
 <!-- All'interno del template ci va un solo figlio -->
 <template> 
   <div>
-    <AppHeader />
+    <header>
+        <h1> 
+            {{ message }}
+        </h1>
+        <img src="../assets/vue.svg" alt="">
+    </header>
   </div>
 </template>
 
 <style scoped>
 header {
     text-align: center;
+}
+
+img{
+  width: 100px;
+  margin: 50px;
 }
 </style>
 
